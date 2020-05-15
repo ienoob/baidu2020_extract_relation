c144bf444547a871df9a8c73e49b3647d76f8e8b
jack.li <jack.li@eisoo.com>
2020-05-15 16:51:24 +0800

    增加数据预处理

---
+++ data_preprocess.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+__author__ = "jack.li"
+
+train_data_path = "D:\\data\\百度比赛\\2020语言与智能技术竞赛：关系抽取任务\\train_data.json"
+dev_data_path = "D:\\data\\百度比赛\\2020语言与智能技术竞赛：关系抽取任务\\dev_data.json"
+schema_data_path = "D:\\data\\百度比赛\\2020语言与智能技术竞赛：关系抽取任务\\schema.json"
+
